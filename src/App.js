7c1ec1b1abec2b6fa77a5306bf5cd02317abf0a4
PVPfiveseconds <pedrovalencaprado@gmail.com>
2020-07-18 01:59:27 -0300

    Finish rendering repositories from storage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Routes from './routes';
+import Repositories from './pages/Repositories';
+import Home from './pages/Home';
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes path='/' component={Home} />,
+    <Routes path='/repositories' component={Repositories} />
   );
 }
 
